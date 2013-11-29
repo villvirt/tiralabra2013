d7c7bee10a671ac302c7360cc4483cb7e868e193
villvirt <ville@mevirtaset.com>
2013-11-29 17:49:15 +0200

    esteet ja random testaus

--- src/tiralabra/IDA.java
+++ src/tiralabra/IDA.java
@@ -6,11 +6,16 @@ public class IDA {
     public Solmu node;
     public int g;
     public int f;
+<<<<<<< Updated upstream
     public int heuristiikka[][];
     public String reitti[][]; 
+=======
+    public String reitti[][];
+>>>>>>> Stashed changes
 
-    public IDA(int[][] verkko, int maali_i, int maali_j, int alku_i, int alku_j) {
+    public IDA(String[][] verkko, int maali_i, int maali_j, int alku_i, int alku_j) {
 
+<<<<<<< Updated upstream
         heuristiikka = laskeHeuristiikka(verkko, maali_i, maali_j);
         reitti = new String[verkko.length][verkko[0].length];
         reitti[maali_i][maali_j]="m";
@@ -43,16 +48,57 @@ public class IDA {
     private int search(int node_i, int node_j, int g, int bound, int maali_i, int maali_j) {
         int t;
         f = g + h(node_i, node_j);
+=======
+        reitti = verkko;
+        reitti[maali_i][maali_j] = "m";
+        double bound = heuristiikka(reitti, alku_i, alku_j, maali_i, maali_j);
+        System.out.println("");
+        for(int i  =0;i<1000;i++){
+        double t = search(alku_i, alku_j, 0, bound, maali_i, maali_j);
+        bound=t;
+        if(t==-1){
+            break;
+        }
+        }
+        /*
+         * if (t == 999999999) { System.out.println("Ei löytynyt"); } if (t ==
+         * -1) {
+         */
+        reitti[alku_i][alku_j] = "s"; //tulostetaa reitti
+        for (int i = 0; i < verkko.length; i++) {
+            for (int j = 0; j < verkko[0].length; j++) {
+                if (reitti[i][j] == null) {
+                    System.out.print("[ ]");
+                } else {
+                    System.out.print("[" + reitti[i][j] + "]");
+                }
+            }
+            System.out.println();
+        }
+        //  }
+
+
+    }
+
+    private double search(int node_i, int node_j, int g, double bound, int maali_i, int maali_j) {  //hakualgoritmi
+        f = g + heuristiikka(reitti, node_i, node_j, maali_i, maali_j);
+>>>>>>> Stashed changes
         if (f > bound) {
             return f;
         }
+                    reitti[node_i][node_j] = "x";
         if (node_i == maali_i && node_j == maali_j) {
             reitti[node_i][node_j] = "m";
             return -1;
         }
-        reitti[node_i][node_j] = "x";
+      
 
+<<<<<<< Updated upstream
         int min = 999999999;  //close enuf
+=======
+        double min = Double.POSITIVE_INFINITY ;  //close enuf
+        // Bugaaaaa
+>>>>>>> Stashed changes
         for (int i = 0; i < 4; i++) {  //ylös, oikea, alas, vasen
             if (i == 0) {
                 if (node_i - 1 >= 0) {
@@ -64,6 +110,9 @@ public class IDA {
                     if (t < min) {
                         min = t;
                     }
+                }
+                if(min==-1){
+                    return min;
                 }
             }
             if (i == 1) {
@@ -77,6 +126,9 @@ public class IDA {
                         min = t;
                     }
                 }
+                if(min==-1){
+                    return min;
+                }
             }
             if (i == 2) {
                 if (node_i + 1 < heuristiikka.length) {
@@ -88,6 +140,9 @@ public class IDA {
                     if (t < min) {
                         min = t;
                     }
+                }
+                if(min==-1){
+                    return min;
                 }
             }
             if (i == 3) {
@@ -101,25 +156,54 @@ public class IDA {
                         min = t;
                     }
                 }
+                if(min==-1){
+                    return min;
+                }
             }
         }
         return min;
     }
 
+<<<<<<< Updated upstream
     private int[][] laskeHeuristiikka(int[][] verkko, int maali_i, int maali_j) {
         for (int i = 0; i < verkko.length; i++) {
             for (int j = 0; j < verkko[0].length; j++) {
                 verkko[i][j] = Math.abs(maali_i - i) + Math.abs(maali_j - j);
+=======
+    private double etene(double min, int node_i, int node_j, int g, double bound, int maali_i, int maali_j) {
+        double t = search(node_i, node_j, g + 1, bound, maali_i, maali_j);
+        if (t == -1) {
+
+            return -1;
+        }
+        if (t < min) {
+            min = t;
+        }
+        return min;
+>>>>>>> Stashed changes
 
                 System.out.print("[" + verkko[i][j] + "]");
 
             }
             System.out.println();
         }
+<<<<<<< Updated upstream
         return verkko;
     }
 
     private int h(int node_x, int node_y) {
         return heuristiikka[node_x][node_y];
+=======
+        if(reitti[node_i][node_j]=="r"){
+            return false;
+        }
+
+        return true;
+    }
+
+    private int heuristiikka(String[][]verkko, int node_i, int node_j, int maali_i, int maali_j) {   //palauttaa halutun arvon
+        
+        return Math.abs(maali_i - node_i) + Math.abs(maali_j - node_j);
+>>>>>>> Stashed changes
     }
 }
--- src/tiralabra/Tiralabra.java
+++ src/tiralabra/Tiralabra.java
@@ -5,11 +5,42 @@ public class Tiralabra {
 
     //testaillaan
     public static void main(String[] args) {
-       int[][] verkko = new int[10][10];
-       int[][] verkko2 = new int[5][5];
-       int[][] verkko3 = new int[2][8];
-        IDA ida = new IDA(verkko, 4, 9, 2, 7);
+      String[][] verkko = new String[10][10];
+       /*String[][] verkko2 = new String[][]{{" "," "," "," "," "},
+                                           {" "," "," "," "," "},
+                                           {" "," "," ","r"," "},
+                                           {" "," "," ","r"," "},
+                                           {" "," "," ","r","r"}
+    };
+       String[][] verkko3 = new String[2][8];
+       
+        IDA ida = new IDA(verkko, 4, 9, 2, 7);      //verkko,maali_i,maali_j,alku_i,alku_j
         ida = new IDA(verkko2, 4, 2, 2, 4);
-       ida = new IDA(verkko3, 1, 3, 1, 5);
+       ida = new IDA(verkko3, 1, 3, 1, 5);*/
+       
+       verkko = new String[rnd(5)+2][rnd(5)+2];
+       int maali_i=rnd(verkko.length);
+       int maali_j=rnd(verkko[0].length);
+       int alku_i=rnd(verkko.length);
+       int alku_j=rnd(verkko[0].length);
+        System.out.println(maali_i);
+        System.out.println(maali_j);
+        System.out.println(alku_i);
+        System.out.println(alku_j);
+       for(int i=0;i<verkko.length;i++){
+           for(int j=0; j<verkko[0].length;j++){
+               if(!(i==maali_i && i==alku_i) || !(i==alku_i && j==alku_j)){
+                   int kivi=rnd(4);
+                   if(kivi==3){
+                       verkko[i][j]="r";
+                       
+                   }
+               }
+           }
+       }
+       IDA ida = new IDA(verkko,maali_i,maali_j,alku_i,alku_j);
+    }
+    private static int rnd(int vali){    
+    return (int)(Math.random() * vali);
     }
 }
